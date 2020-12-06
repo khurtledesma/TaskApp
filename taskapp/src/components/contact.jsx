a8076c4a7baac0b85c50f508cdc64e8104b6f17c
Khurt Ledesma <khurt.ledesma@gmail.com>
2020-12-06 13:33:36 -0600

    updated basic contact page

--- taskapp/src/components/contact.jsx
+++ taskapp/src/components/contact.jsx
@@ -1,7 +1,29 @@
 import React from "react";
 
 const Contact = () => {
-    return <h2>contact info here</h2>
+    return (
+    <div class="mainApp">
+        <h1>Contact Us!</h1>
+        <form>
+            <fieldset>
+                <label for="name">Name:</label>
+                <input type="text" name="name"></input>
+            </fieldset>
+
+            <fieldset>
+                <label for="email">Email:</label>
+                <input type="email" name="email"></input>
+            </fieldset>
+
+            <fieldset>
+                <label for="message">Message:</label>
+                <input type="text" name="message"></input>
+            </fieldset>
+            <button type="submit"> Send! </button>
+        </form>
+    </div>
+    
+    )
 }
 
 export default Contact;
