78d5d56011a01b4a60de09be5c5595ffaa4c1758
Khurt Ledesma <khurt.ledesma@gmail.com>
2020-12-28 11:18:21 -0600

    updated contact form fields

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -6,15 +6,15 @@ const Contact = () => {
         <h1>Contact Us!</h1>
         <form>
             <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="Name:" name="name"></input>
+                <input type="text" class="form-control" placeholder="Name" name="name"></input>
             </fieldset>
 
             <fieldset class="form-group">
-                <input type="email" class="form-control" placeholder="Email:"  name="email"></input>
+                <input type="email" class="form-control" placeholder="Email"  name="email"></input>
             </fieldset>
 
             <fieldset class="form-group">
-                <input type="text" class="form-control" placeholder="Enter message here" name="message"></input>
+                <input type="text" class="form-control" placeholder="Enter message here" name="message" height="500px" id="messageInput"></input>
             </fieldset>
             <button type="submit" class="btn btn-block btn-success"> Send! </button>
         </form>
