46b74568dc5324967574721b456b7ecda763a874
Khurt Ledesma <khurt.ledesma@gmail.com>
2021-01-12 20:23:04 -0600

    remove bootstrap

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import "./css/bootstrap.min.css";
+
 import "./css/App.css"
 
 ReactDOM.render(
